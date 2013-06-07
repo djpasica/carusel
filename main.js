b7092a240b4dbe8b88ca1df03ef0f349421a0ba7
djpasica <djpasica92@gmail.com>
2013-06-07 16:02:47 +0300

    Alpha версія JS

--- main.js
+++ main.js
@@ -1,57 +1,38 @@
-var elementsAmount = 3; //скільки виводим
+var elementsAmount = 6; //скільки виводим
 var elements = ["Element 1", "Element 2", "Element 3", "Elemnt 4", "Element 5", "Element 6", "Element 7", "Element 8"];
-//var elements = ["Element 1", "Element 2", "Element 3"];
-var slicedElements = [];
-var startPosition = 0;
 var elementsBlock = null;
 
-function init() {
-	
+function init() {	
 	$(elementsBlock).empty();
 	
-	for (var index in slicedElements) {
-		$(elementsBlock).append('<div class="inline element">'+slicedElements[index]+'</div>');
+	for (var i = 0; i < elementsAmount; i++) {
+		$(elementsBlock).append('<div class="inline element">'+elements[i]+'</div>');
 	}
 }
 
-function generateList(startPosition) {
-	var endNum = elementsAmount + startPosition;
-	console.log("endNum: "+endNum);
-	console.log("startPosition: "+startPosition);
-	slicedElements = elements.slice(startPosition ,endNum);
+function generateList(pos) {
 	
-	if (endNum > elements.length) {
-		//додаєм елементи з початку списку
-		var num = endNum - elements.length;
-		slicedElements = slicedElements.concat(elements.slice(0, num));
+	var removedElement;
+	
+	if (pos < 0) {
+		removedElement = elements[elements.length-1];
+		elements = elements.slice(0,elements.length-1);
+		elements = [removedElement].concat(elements);
 	}
-	else if (endNum < elementsAmount) {
-		//додаєм елементи з кінця списку
-		
+	else {
+		removedElement = elements[0];
+		elements = elements.concat(removedElement);
+		elements = elements.slice(1,elements.length);
 	}
 }
 
 $(document).ready(function() {
 	
 	elementsBlock = ".elements-content";
-	generateList(startPosition);
 	init();
 	
 	$(".button-control").click(function(){
-		
-		if ($(this).hasClass("prew")) {
-			startPosition--;
-		}
-		else {
-			startPosition++;
-		}
-		
-		if (startPosition > elements.length) {
-			startPosition = 1;
-		}
-		
-		generateList(startPosition);
+		generateList($(this).hasClass("prew") ? -1 : 1);
 		init();
-		console.log("---------------");
 	});
 });
