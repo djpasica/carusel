18de54a6ab17d5e1e1f75efd20773996c54a48b9
djpasica <djpasica92@gmail.com>
2013-06-07 15:24:34 +0300

    Правки JS

--- main.js
+++ main.js
@@ -1,56 +1,57 @@
-var elementsAmount = 5;
+var elementsAmount = 3; //скільки виводим
 var elements = ["Element 1", "Element 2", "Element 3", "Elemnt 4", "Element 5", "Element 6", "Element 7", "Element 8"];
-var currentElements = [];
-var firstElemet = 0;
+//var elements = ["Element 1", "Element 2", "Element 3"];
+var slicedElements = [];
+var startPosition = 0;
+var elementsBlock = null;
 
-function init() {	
-	$(".elements-content").empty();
+function init() {
 	
-	for (var i = 0; i < elementsAmount; i++) {
-		$(".elements-content").append('<div class="inline element">'+currentElements[i]+'</div>');
+	$(elementsBlock).empty();
+	
+	for (var index in slicedElements) {
+		$(elementsBlock).append('<div class="inline element">'+slicedElements[index]+'</div>');
 	}
 }
 
-function generateList(newCurrent) {
-	console.log("newCurrent: "+newCurrent);
-	currentElements = elements.slice(newCurrent,elementsAmount+newCurrent);
+function generateList(startPosition) {
+	var endNum = elementsAmount + startPosition;
+	console.log("endNum: "+endNum);
+	console.log("startPosition: "+startPosition);
+	slicedElements = elements.slice(startPosition ,endNum);
 	
-	console.log("newCurrent: "+currentElements);
+	if (endNum > elements.length) {
+		//додаєм елементи з початку списку
+		var num = endNum - elements.length;
+		slicedElements = slicedElements.concat(elements.slice(0, num));
+	}
+	else if (endNum < elementsAmount) {
+		//додаєм елементи з кінця списку
+		
+	}
 }
 
-$(document).ready(function(){
+$(document).ready(function() {
 	
-	firstElemet = 0;
-	generateList(firstElemet);
+	elementsBlock = ".elements-content";
+	generateList(startPosition);
 	init();
 	
 	$(".button-control").click(function(){
-		console.log("cliked");
-		
-		var position = 1;
-		var delElement = null;
 		
 		if ($(this).hasClass("prew")) {
-			position = -1;
+			startPosition--;
+		}
+		else {
+			startPosition++;
 		}
 		
-		firstElemet += position; 
+		if (startPosition > elements.length) {
+			startPosition = 1;
+		}
 		
-		generateList(firstElemet);
+		generateList(startPosition);
 		init();
-		
-//		if (position < 1) {
-//			delElement = $(".elements-content .element:first");
-//		}
-//		else {
-//			delElement = $(".elements-content .element:last");
-//		}
-//		
-//		delElement.remove();
-		
-		
-		
-		console.log("end");
+		console.log("---------------");
 	});
-	
 });
